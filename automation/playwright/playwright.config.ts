a3b30fabd5ba4539f01492f7feff5525445ebddb
DeadDraw613 <248383597+DeadDraw613@users.noreply.github.com>
2026-07-13 21:19:19 -0400

    got playwright running on lab test machine

--- automation/playwright/playwright.config.ts
+++ automation/playwright/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
       /* Base URL to use in actions like `await page.goto('/')`. */
-      baseURL: "http://192.168.59.131:8000/",
+      baseURL: "http://192.168.70.89/",
       testIdAttribute: "data-test",
       /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
       trace: "on",
