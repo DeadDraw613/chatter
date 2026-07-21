e4acb82235ad77f1e8417ce2276182b4f4091b28
DeadDraw613 <248383597+DeadDraw613@users.noreply.github.com>
2026-07-21 18:21:32 -0400

    chat page POM added

---
+++ automation/playwright/lib/pages/chat-page-pom.ts
@@ -0,0 +1,19 @@
+import { Locator, Page } from "@playwright/test";
+
+export class ChatPage {
+
+    public readonly navChatLink;
+    public readonly connectionUserDoug;
+    public readonly messageInputBox;
+    public readonly sendMessageButton;
+
+    constructor(page: Page) {
+        this.navChatLink = page.getByRole('link', { name: 'Chat' });
+        this.connectionUserDoug = page.getByRole('link', { name: 'Doug doug@doug.com' });
+        this.messageInputBox = page.getByRole('textbox', { name: 'Type your message...' });
+        this.sendMessageButton = page.getByRole('button', { name: '📩' });    
+}
+
+//   await .fill('Playwright sent this message');
+//   await page.getByRole('button', { name: '📩' }).click();
+//   await expect(page.getByText('Playwright sent this message')).toBeVisible();
