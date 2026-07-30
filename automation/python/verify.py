cfbf456116f8ac3328f88347268681d596dc9e40
DeadDraw613 <248383597+DeadDraw613@users.noreply.github.com>
2026-07-29 22:45:06 -0400

    changes to verify.py and environment variable names

--- automation/python/verify.py
+++ automation/python/verify.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 base_url = os.getenv("CHATTER_URL")
-username = os.getenv("CHATTER_USER")
+username = os.getenv("CHATTER_USERNAME")
 
 print("requests:", requests.__version__)
 print("json imported successfully")
